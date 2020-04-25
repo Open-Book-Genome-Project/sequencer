857265761eba58639fb9b7533a2adba9d7869a91
Ishank Sharma <ishankdev@gmail.com>
2020-04-26 01:08:46 +0530

    make changes

--- bgp/modules/terms.py
+++ bgp/modules/terms.py
@@ -148,10 +148,7 @@ class CopyrightPageDetectorModule:
     
     def search(self, keyword):
         parser = etree.XMLParser(recover=True)
-        tree = etree.parse('djvu.xml',parser)
-        # search is case sensitive "africa" wont be detected
-        # keyword = "AFRICA"
-        #Iterating over each tag called OBJECT
+        tree = etree.parse('djvu.xml',parser) # Help required: This xml file needs to be fetched through OL python library
         for x in tree.iter('OBJECT'):
             # Iterating over each WORD tag to locate the keyword
             for i in x.iter('WORD'):
@@ -161,9 +158,8 @@ class CopyrightPageDetectorModule:
                         splitting at '.' and finding the last 4 lettes of the book name in this case it is 0273
                     '''
                     param = x[0].attrib['value'].split('.')[0] 
-                    param = param[-4:]
+                    pageNo = param[-4:]
                     
-                    print("Keyword",i.text,"found at","page",param)
 
     @property
     def results(self):
