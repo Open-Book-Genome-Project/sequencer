75b8482a513cd7cf5e8f517bea6a7ce40199fec3
Mek <michael.karpeles@gmail.com>
2020-07-16 02:33:58 -0700

    Feature/variable page type modules (#15)
    * adds tests, enables extensible pagetype detection
    * fixes unicode issues
    
    Co-authored-by: mek <mek@archive.org>

--- bgp/modules/pagetypes.py
+++ bgp/modules/pagetypes.py
@@ -4,34 +4,7 @@ import string
 import re
 from collections import defaultdict
 from lxml import etree
-
-
-STOP_WORDS = set("""'d 'll 'm 're 's 've a about above across after afterwards
-again against all almost alone along already also although always am among
-amongst amount an and another any anyhow anyone anything anyway anywhere are
-around as at back be became because become becomes becoming been before
-beforehand behind being below beside besides between beyond both bottom but by
-ca call can cannot could did do does doing done down due during each eight
-either eleven else elsewhere empty enough even ever every everyone everything
-everywhere except few fifteen fifty first five for former formerly forty four
-from front full further get give go had has have he hence her here hereafter
-hereby herein hereupon hers herself him himself his how however hundred i if
-in indeed into is it its itself just keep last latter latterly least less made
-make many may me meanwhile might mine more moreover most mostly move much must
-my myself n't name namely neither never nevertheless next nine no nobody none
-noone nor not nothing now nowhere n‘t n’t of off often on once one only onto
-or other others otherwise our ours ourselves out over own part per perhaps
-please put quite rather re really regarding same say see seem seemed seeming
-seems serious several she should show side since six sixty so some somehow
-someone something sometime sometimes somewhere still such take ten than that
-the their them themselves then thence there thereafter thereby therefore
-therein thereupon these they third this those though three through throughout
-thru thus to together too top toward towards twelve twenty two under unless
-until up upon us used using various very via was we well were what whatever
-when whence whenever where whereafter whereas whereby wherein whereupon
-wherever whether which while whither who whoever whole whom whose why will
-with within without would yet you your yours yourself yourselves ‘d ‘ll ‘m ‘re
-‘s ‘ve ’d ’ll ’m ’re ’s ’ve""".split())
+from bgp.utils import STOP_WORDS
 
 
 class NGramProcessor():
@@ -65,12 +38,12 @@ class NGramProcessor():
                            punctuation='!"#$%&\'()*+,;<=>?@[\\]^`{|}*'):
         stop_words = stop_words or {}
         def clean(fulltext):
-            return ''.join(c.encode("ascii", "ignore") for c in (
+            return ''.join(c for c in (
                 fulltext.lower()
                 .replace('. ', ' ')
                 .replace('\n-', '')
                 .replace('\n', ' ')
-            ) if c not in punctuation)
+            ) if str(c) not in punctuation)
         tokens = [t.strip() for t in clean(fulltext).split(' ') if t and t not in stop_words]
         return cls.tokens_to_ngrams(tokens, n=n) if n > 1 else tokens
 
--- bgp/test.py
+++ bgp/test.py
@@ -4,10 +4,16 @@ import unittest
 from bgp import ia
 from bgp.runner import DEFAULT_SEQUENCER
 
+
 book = ia.get_item('9780262517638OpenAccess')
 genome = DEFAULT_SEQUENCER.sequence(book)
 
+
 class TestSequencer(unittest.TestCase):
 
+    def test_smoketest(self):
+        book = ia.get_item(itemid)
+        genome = DEFAULT_SEQUENCER.sequence('hpmor')
+    
     def test_copyright_page_detection(self):
         assert genome.results['pagetypes']['copyright_page'][0] == '0004'
