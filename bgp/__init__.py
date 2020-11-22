21fce38c18fd3495f6b995b614d82926adfdf2cb
Christian Clauss <cclauss@me.com>
2020-11-22 10:01:39 +0100

    isort the imports and pyupgrade to modern idioms

--- bgp/__init__.py
+++ bgp/__init__.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 """
     __init__.py
     ~~~~~~~~~~~
--- bgp/config.py
+++ bgp/config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
     :copyright: (c) 2020 by Open Book Genome Project
