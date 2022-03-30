63c2a99e4d8d1ffe755eb76f2236e038d83e02c9
Teo J Cheng <teofiredragon@gmail.com>
2022-03-30 14:47:30 -0700

    fixing ngrams - consolidated `STOP_WORDS` - increased threshold on 2grams - adding a 3gram processor

--- bgp/bgp_test.py
+++ bgp/bgp_test.py
@@ -1,6 +1,8 @@
 import unittest
 
 from bgp import ia
+from bgp.utils import STOP_WORDS
+from bgp.modules.terms import NGramProcessor
 
 try:  # TODO: create bgp.runner
     from bgp import DEFAULT_SEQUENCER
@@ -27,3 +29,8 @@ class TestSequencer(unittest.TestCase):
             'isbns': ['9780262517638'],
             'page': '0004'
         }
+
+    def test_stop_words(self):
+        book = "be be water be a small a any anyhow anyone 2 3 4 5 anything anyway anywhere are around at back be became because become becomes becoming been before beforehand behind being below beside besides between beyond both bottom but by call can cannot could did do does doing melon be"
+        ngrams = NGramProcessor.fulltext_to_ngrams(book, n=2, stop_words=STOP_WORDS)
+        assert ngrams == ['water small', 'small melon'], ngrams
